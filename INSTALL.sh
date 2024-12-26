26f566fb11aaaaacc8f518e4e164c33ac99468a6
Cong Luan Tran <congluan.tran19@gmail.com>
2024-12-26 14:32:21 +0700

    add installtion script

---
+++ INSTALL.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Use cargo to install the packages, the default installation is $Home/.cargo/bin
+cargo install --path .
+# Create a symlink to /usr/local/bin because systemd service should have static path
+sudo ln -s "$HOME/.cargo/bin/myfancontrol" /usr/local/bin
+# Copy the service file into the service file directory of systemd
+sudo cp myfancontrol.service /etc/systemd/system
+
+# Reload systemd daemon
+sudo systemctl daemon-reload
+# Enable the service
+sudo systemctl enable myfancontrol
+# Start it now
+sudo systemctl start myfancontrol
