63bf56065487bbc65a8a0cc5345ac0d10c386cb4
Cong Luan Tran <congluan.tran19@gmail.com>
2025-07-17 15:25:29 +0700

    fix: fix noob bash mistakes

--- INSTALL.sh
+++ INSTALL.sh
@@ -3,8 +3,8 @@
 # Use cargo to install the packages, the default installation is $Home/.cargo/bin
 cargo install --path .
 # Create a symlink to /usr/local/bin because systemd service should have static path
-if [ -z "$(find /usr/local/bin)" ]; then
-    sudo ln -s "$HOME/.cargo/bin/myfancontrol" /usr/local/bin
+if [[ -z "$(find /usr/local/bin)" ]]; then
+    sudo ln -s "$HOME/.cargo/bin/fanctl" /usr/local/bin
 fi
 # Copy the service file into the service file directory of systemd
 sudo cp fanctl.service /etc/systemd/system
